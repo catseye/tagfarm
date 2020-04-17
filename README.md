87407582603156ab11fb9fc22dab2d6dad3a8bdd
Chris Pressey <cpressey@catseye.tc>
2020-04-17 09:54:41 +0100

    Initial import of files for tagfarm distribution.

---
+++ README.md
@@ -0,0 +1,133 @@
+`tagfarm`
+=========
+
+_Version 0.1_
+| _Entry_ [@ catseye.tc](https://catseye.tc/node/shelf)
+| _See also:_ [shelf](https://github.com/catseye/shelf)
+∘ [ellsync](https://github.com/catseye/ellsync)
+∘ [yastasoti](https://github.com/catseye/yastasoti)
+
+- - - -
+
+<img align="right" src="images/tagfarm-logo.png?raw=true" />
+
+**tagfarm** is a lightweight filesystem-based categorization system for media files.
+
+Motivation
+----------
+
+The limitation of a hierarchical filesystem is that any one file can only reside in one
+directory.  But most files are best described by classifying them in more than one
+category.  So directories don't map very well to categories -- if you have a picture of
+the Mona Lisa, should you put it in `Portraits` or `Works by Leonardo da Vinci` or
+`Enigmatic Smiles`?
+
+The solution on blogs and wikis is to use "tags" or "categories", to allow every page
+to be tagged with zero or more tags, and to allow all pages with a given tag to be listed
+in a "tag index" or "category page".
+
+tagfarm implements the same idea on a local filesystem.  Each tag is implemented as a
+directory containing symbolic links to the files that have that tag.  And this is *all*
+it consists of.
+
+There are several advantages to this.  There's no metadata to go out of sync, no database
+engine to install and maintain.  When you move files around, you can just run
+`tagfarm repair` to rewrite the tag links.  You can treat the tag links as you would any
+other file — for example, you can remove a tag from a file just by deleting the tag link.
+
+There are also some disadvantages, of course.  Primarily, any limitations that your
+filesystem has are also going to be imposed on the categorization system.  So, for
+example, if your OS has performance problems listing 10,000 files in a single directory,
+the same would hold for a set of 10,000 files that are tagged with the same tag.
+
+Quick start
+-----------
+
+Make sure you have Python installed, clone this repository, and put its `bin` directory on
+your executable search path.  You can then run `tagfarm` from your terminal.
+
+Overview
+--------
+
+### Media tree
+
+`tagfarm` operates on a part of your filesystem it calls the *media tree*.  There may be
+multiple media trees in your filesystem.  A media tree is identified by having a directory
+called `by-tag` in it.  When `tagfarm` is started, it finds the media tree it will operate
+on, by looking for the `by-tag` directory, first in the current directory, then in every
+successive parent directory thereof.  If it reaches `/` without having found a `by-tag`
+directory, it exits immediately with an error code.
+
+### `tag` and `untag`
+
+You can add a tag to one or more files with the command
+
+    tagfarm tag <tagname> <filename> [<filename2>...]
+
+You can then list the contents of `by-tag/<tagname>` and see there are symlinks there.
+
+You can remove a tag to one or more files with the command
+
+    tagfarm untag <tagname> <filename> [<filename2>...]
+
+### `showtags`
+
+You can list the tags that have been applied to one or more files with
+
+    tagfarm showtags <filename> [<filename2>...]
+
+### `repair`
+
+If the source files are moved around, the symbolic links will break.  Assuming the
+files, after having moved, have not changed names and are still found somewhere in
+the media tree, tagfarm can repair the broken links with the command
+
+    tagfarm repair
+
+By default, tagfarm will only attempt to repair broken links if they are actually
+symlinks (not, for example, regular files) and not broken.  To have it replace all files
+it happens to find in the tag link directory, pass `--force-relink`.  This is occasionally
+handy for converting a directory full of copies of elsewhere-existing media files,
+into links.
+
+`tagfarm repair` will also replace any links it finds that have absolute target paths,
+with ones with relative target paths, even when the link is not broken.
+
+`tagfarm repair` will also, when processing a link whose name is like `Link to xyz`,
+rename it to simply `xyz`.  This is to handle the case where links are manually
+created in a tag directory using a tools such as Nautilus (Gnome Desktop).
+
+### `rename`
+
+    tagfarm rename <oldfilename> <newfilename>
+
+Renames the file, like `mv`, but also updates any tags that might be on it.
+
+### `collect`
+
+    tagfarm collect <tagname> <destdir>
+
+Convenience command to move all the files with a given tag into a destination directory.
+If the destination directory does not yet exist, will be created.  The tags of the files
+are not changed.  At the end, `tagfarm repair` is called to update the tag links.
+
+Other operations
+----------------
+
+The advantage of tagfarm being ultra-lightweight is that if there is something that
+it does not directly support, it's often easy to accomplish it by simply issuing
+some conventional commands to alter the filesystem.  For this reason, some
+functionalities you might expect to exist, don't have specific `tagfarm` commands
+implemented for them.
+
+For example, to rename a tag, one needs only to rename the directory that contains
+the tag links.  For example:
+
+    mv by-tag/airplane by-tag/aeroplane
+
+TODO
+----
+
+Unit tests.
+
+Better handling of cases where the target being linked is itself a link.
---
+++ src/tagfarm/main.py
@@ -0,0 +1,276 @@
+# encoding: UTF-8
+
+from argparse import ArgumentParser
+import errno
+import os
+import sys
+from subprocess import Popen, STDOUT, PIPE
+
+
+# - - - -
+
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+
+def find_media_root(path):
+    while path != '/':
+        if os.path.isdir(os.path.join(path, 'by-tag')):
+            return path
+        path = os.path.abspath(os.path.join(path, os.pardir))
+    raise ValueError("could not locate `by-tag` directory in curent directory or any parent directory thereof")
+
+
+def index_files(media_root):
+    index = {}
+    for root, dirs, files in os.walk(media_root):
+        if os.path.normpath(root) == os.path.normpath(os.path.join(media_root, 'by-tag')):
+            dirs[:] = []
+            continue
+
+        for basename in files:
+            filename = os.path.normpath(os.path.join(root, basename))
+            if os.path.islink(filename):
+                continue
+
+            index.setdefault(basename, set()).add(filename)
+    return index
+
+
+def is_broken_link(path):
+    return os.path.lexists(path) and not os.path.exists(path)
+
+
+def is_absolute_link(path):
+    return os.path.lexists(path) and os.readlink(path).startswith('/')
+
+
+def tag_file(media_root, filename, tag):
+    mkdir_p(os.path.join(media_root, 'by-tag', tag))
+    linkname = os.path.join(media_root, 'by-tag', tag, os.path.basename(filename))
+    if not os.path.lexists(linkname):
+        srcname = os.path.join('..', '..', os.path.relpath(filename, media_root))
+        os.symlink(srcname, linkname)
+
+
+def perform_repair(media_root, verbose=False, force_relink=False, prune=False):
+    index = index_files(media_root)
+
+    by_tags_dir = os.path.join(media_root, 'by-tag')
+    for tag in os.listdir(by_tags_dir):
+        tagdir = os.path.join(by_tags_dir, tag)
+        if not os.path.isdir(tagdir):
+            continue
+        print('*** {}'.format(tag))
+        for basename in os.listdir(tagdir):
+
+            linkname = os.path.join(tagdir, basename)
+
+            if basename.startswith('Link to '):
+                new_basename = basename[8:]
+                new_linkname = os.path.join(tagdir, new_basename)
+                if os.path.lexists(new_linkname):
+                    print("WARNING: not renaming '{}' because '{}' already exists".format(linkname, new_linkname))
+                else:
+                    print("RENAMING {} -> {}".format(linkname, new_linkname))
+                    os.rename(linkname, new_linkname)
+                    linkname = new_linkname
+
+            if not force_relink:
+                if not os.path.islink(linkname):
+                    print("WARNING: skipping {} (is regular file, but --force-relink not given)".format(linkname))
+                    continue
+                if not (is_broken_link(linkname) or is_absolute_link(linkname)):
+                    if verbose:
+                        print('kept {} -> {}'.format(linkname, os.readlink(linkname)))
+                    continue
+
+            candidates = index.get(basename, set())
+            if len(candidates) == 0:
+                if prune:
+                    print("NOTICE: no candidates for {}, DELETING".format(basename))
+                    os.remove(linkname)
+                else:
+                    print("WARNING: no candidates for {}".format(basename))
+            elif len(candidates) > 1:
+                print("WARNING: multiple candidates for {}:  {}".format(basename, candidates))
+            else:
+                os.remove(linkname)
+                filename = list(candidates)[0]
+                srcname = os.path.join('..', '..', os.path.relpath(filename, media_root))
+                os.symlink(srcname, linkname)
+                print('FIXED {} -> {}'.format(linkname, srcname))
+
+
+# - - - - commands - - - -
+
+
+def tag(media_root, args, verbose=False):
+    argparser = ArgumentParser()
+    argparser.add_argument('tag', metavar='TAG', type=str,
+        help='Name of tag to apply'
+    )
+    argparser.add_argument('filenames', metavar='FILENAME', type=str, nargs='+',
+        help='Names of files to tag'
+    )
+    options = argparser.parse_args(args)
+
+    for filename in options.filenames:
+        tag_file(media_root, filename, options.tag)
+
+
+def untag(media_root, args, verbose=False):
+    argparser = ArgumentParser()
+    argparser.add_argument('tag', metavar='TAG', type=str,
+        help='Name of tag to remove'
+    )
+    argparser.add_argument('filenames', metavar='FILENAME', type=str, nargs='+',
+        help='Names of files to untag'
+    )
+    options = argparser.parse_args(args)
+
+    for filename in options.filenames:
+        linkname = os.path.join(media_root, 'by-tag', options.tag, os.path.basename(filename))
+        if os.path.lexists(linkname):
+            os.remove(linkname)
+
+
+def showtags(media_root, args, verbose=False):
+    argparser = ArgumentParser()
+    argparser.add_argument('filenames', metavar='FILENAME', type=str, nargs='+',
+        help='Names of files to show tags of',
+    )
+    argparser.add_argument('--show-only-fewer-than', type=int, default=None,
+        help='If given, report only those files that have fewer than this number of tags',
+    )
+    options = argparser.parse_args(args)
+
+    by_tags_dir = os.path.join(media_root, 'by-tag')
+
+    for filename in options.filenames:
+        tags = []
+        basename = os.path.basename(os.path.normpath(filename))
+        for tag in sorted(os.listdir(by_tags_dir)):
+            linkname = os.path.join(by_tags_dir, tag, basename)
+            if os.path.lexists(linkname):
+                tags.append(tag)
+        if options.show_only_fewer_than is None or len(tags) < options.show_only_fewer_than:
+            print('{}: {}'.format(filename, ', '.join(tags)))
+
+
+def repair(media_root, args, verbose=False):
+    argparser = ArgumentParser()
+    argparser.add_argument('--force-relink', action='store_true',
+        help='Replace files found in taglinks directory even when they are not symlinks or not broken'
+    )
+    argparser.add_argument('--prune', action='store_true',
+        help='Remove broken symlinks for which no candidate files can be found'
+    )
+    options = argparser.parse_args(args)
+
+    perform_repair(media_root, verbose=verbose, force_relink=options.force_relink, prune=options.prune)
+
+
+def rename(media_root, args, verbose=False):
+    argparser = ArgumentParser()
+    argparser.add_argument('src', metavar='FILENAME', type=str,
+        help='Current name of file that is to be renamed'
+    )
+    argparser.add_argument('dest', metavar='FILENAME', type=str,
+        help='New name of file'
+    )
+    options = argparser.parse_args(args)
+
+    src = os.path.normpath(options.src)
+    dest = os.path.normpath(options.dest)
+
+    os.rename(src, dest)
+
+    src_basename = os.path.basename(src)
+    dest_basename = os.path.basename(dest)
+
+    by_tags_dir = os.path.join(media_root, 'by-tag')
+    for tag in os.listdir(by_tags_dir):
+        old_linkname = os.path.join(by_tags_dir, tag, src_basename)
+        if os.path.lexists(old_linkname):
+            os.remove(old_linkname)
+            target = os.path.join('..', '..', os.path.relpath(dest, media_root))
+            new_linkname = os.path.join(by_tags_dir, tag, dest_basename)
+            os.symlink(target, new_linkname)
+            print('UPDATED {} -> {}'.format(new_linkname, target))
+
+
+def collect(media_root, args, verbose=False):
+    argparser = ArgumentParser()
+    argparser.add_argument('tag', metavar='TAG', type=str,
+        help='Tag to select files by'
+    )
+    argparser.add_argument('dest', metavar='DIRNAME', type=str,
+        help='Directory to move files into'
+    )
+    options = argparser.parse_args(args)
+
+    tagdir = os.path.join(media_root, 'by-tag', options.tag)
+    if not os.path.isdir(tagdir):
+        print("WARNING: no files tagged '{}'".format(options.tag))
+        return
+
+    dest = os.path.normpath(options.dest)
+    mkdir_p(dest)
+
+    for basename in os.listdir(tagdir):
+        linkname = os.path.join(tagdir, basename)
+
+        filename = os.path.join(tagdir, os.readlink(linkname))
+        new_filename = os.path.join(dest, basename)
+
+        if verbose:
+            print(filename, new_filename)
+
+        if not os.path.exists(new_filename):
+            os.rename(filename, new_filename)
+        else:
+            print("WARNING: {} already exists, not moving".format(new_filename))
+
+    perform_repair(media_root, verbose=verbose)
+
+
+COMMANDS = {
+    'tag': tag,
+    'untag': untag,
+    'showtags': showtags,
+    'repair': repair,
+    'rename': rename,
+    'collect': collect,
+}
+
+# - - - - driver - - - -
+
+
+def main(args):
+    argparser = ArgumentParser()
+
+    argparser.add_argument('command', metavar='COMMAND', type=str,
+        help='The action to take. One of: {}'.format(', '.join(COMMANDS.keys()))
+    )
+    argparser.add_argument('--verbose', action='store_true',
+        help='Produce more reporting output'
+    )
+
+    options, remaining_args = argparser.parse_known_args(args)
+
+    media_root = find_media_root(os.path.realpath('.'))
+
+    command = COMMANDS.get(options.command, None)
+    if command:
+        command(media_root, remaining_args, verbose=options.verbose)
+    else:
+        argparser.print_help()
+        sys.exit(1)
