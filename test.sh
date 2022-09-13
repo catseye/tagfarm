9811a28837c96ebc88b43ccb37ee862ff36384b6
Chris Pressey <cpressey@catseye.tc>
2022-09-13 14:20:14 +0100

    Test under Python 2, or 3, or both, depending on what's available.

--- test.sh
+++ test.sh
@@ -1,4 +1,26 @@
 #!/bin/sh
 
-PYTHONPATH=src python2 src/tagfarm/tests.py || exit 1
-PYTHONPATH=src python3 src/tagfarm/tests.py || exit 1
+if [ "x$PYTHON" != "x" ]; then
+    if command -v "$PYTHON" > /dev/null 2>&1; then
+        PYTHONPATH=src $PYTHON src/tagfarm/tests.py || exit 1
+    else
+        echo "$PYTHON not found on executable search path. Aborting."
+        exit 1
+    fi
+else
+    MISSING=""
+    if command -v python2 > /dev/null 2>&1; then
+        PYTHONPATH=src python2 src/tagfarm/tests.py || exit 1
+    else
+        MISSING="${MISSING}2"
+    fi
+    if command -v python3 > /dev/null 2>&1; then
+        PYTHONPATH=src python3 src/tagfarm/tests.py || exit 1
+    else
+        MISSING="${MISSING}3"
+    fi
+    if [ "x${MISSING}" = "x23" ]; then
+        echo "Neither python2 nor python3 found on executable search path. Aborting."
+        exit 1
+    fi
+fi
